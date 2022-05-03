b668af7af2c3d28abc72c87ab0e6fd418e49930a
JudeSeo <gn0408@gmail.com>
2022-05-03 11:13:07 +0900

    To edit way to show summary

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ function Movie({id, coverImg, title, summary, genres}) {
         <div>
             <img alt={title} src={coverImg}/>
             <h2><Link to={`/movie/${id}`}>{title}</Link></h2>
-            <p>{summary}</p>
+            <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
             <ul>
                 {genres.map(g => <li key={g}>{g}</li>)}
             </ul>
@@ -15,7 +15,7 @@ function Movie({id, coverImg, title, summary, genres}) {
 }
 
 Movie.propTypes = {
-    id:PropTypes.number.isRequired,
+    id: PropTypes.number.isRequired,
     coverImg: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
