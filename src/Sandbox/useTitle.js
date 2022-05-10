f62a51a7d94af24340d7656f8e8a13434dedbe51
JudeSeo <gn0408@gmail.com>
2022-05-10 15:51:33 +0900

    To create useTitle

---
+++ src/Sandbox/useTitle.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const useTitle = (initialTitle) => {
+    const [title, setTilte] = useState(initialTitle);
+    const updateTitle = () => {
+        const htmlTitle = document.querySelector("title");
+        htmlTitle.innerText = title;
+    };
+    useEffect(updateTitle, [title]);
+    return setTilte;
+};
+const App = () => {
+    const titleUpdater = useTitle("Loading...");
+    setTimeout(() => titleUpdater("Home"), 5000);
+    return (
+        <div className="App">
+            <h1>Hi</h1>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
