f24d2fda06cbf198f263bd0637cec49e5443f42d
JudeSeo <gn0408@gmail.com>
2022-05-11 14:05:25 +0900

    To create useClick

---
+++ src/Sandbox/useClick.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, useRef } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const useClick = (onClick) => {
+    const element = useRef();
+    useEffect(() => {
+        if (element.current) element.current.addEventListener("click", onClick);
+
+        return () => {
+            if (element.current)
+                element.current.removeEventListener("click", onClick);
+        };
+    }, []);
+    return element;
+};
+const App = () => {
+    const sayHello = () => console.log("say Hello");
+    const title = useClick(sayHello);
+    return (
+        <div className="App">
+            <h1 ref={title}>Hi</h1>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
