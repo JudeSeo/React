7e67677315f6a9107865debc32eb1b8b0ec58acb
JudeSeo <gn0408@gmail.com>
2022-05-11 14:12:26 +0900

    To create usePreventLeave

---
+++ src/Sandbox/usePreventLeave.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const usePreventLeave = () => {
+    const listener = (event) => {
+        event.preventDefault();
+        event.returnValue = "";
+    };
+    const enablePrevent = () => window.addEventListener("beforeunload", listener);
+    const disablePrevent = () =>
+        window.addEventListener("beforeunload", listener);
+    return { enablePrevent, disablePrevent };
+};
+const App = () => {
+    const { enablePrevent, disablePrevent } = usePreventLeave();
+    return (
+        <div className="App">
+            <button onClick={enablePrevent}>Protect</button>
+            <button onClick={disablePrevent}>Unprotect</button>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
