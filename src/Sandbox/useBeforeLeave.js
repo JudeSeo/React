2d646f369d0adc42c2d179df560dce45016c532d
JudeSeo <gn0408@gmail.com>
2022-05-11 15:59:35 +0900

    To create useBeforeLeave

---
+++ src/Sandbox/useBeforeLeave.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const useBeforeLeave = (onBefore) => {
+    if (typeof onBefore !== "function") return;
+    const handle = (event) => {
+        const { clientY } = event;
+        if (clientY <= 0) onBefore();
+    };
+    useEffect(() => {
+        document.addEventListener("mouseleave", handle);
+        return () => document.removeEventListener("mouseleave", handle);
+    }, []);
+};
+
+const App = () => {
+    const begForLife = () => console.log("Please don't leave");
+    useBeforeLeave(begForLife);
+    return (
+        <div className="App">
+            <h1>Hello</h1>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
