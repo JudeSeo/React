c0470e59d99496fc45b5ba1efd4b9f81a2ebc484
JudeSeo <gn0408@gmail.com>
2022-05-11 14:05:07 +0900

    To create useConfirm

---
+++ src/Sandbox/useConfirm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const useConfirm = (message = "", onConfirm, onCancel) => {
+    if (!onConfirm || typeof onConfirm !== "function") return;
+    if (onCancel && typeof onCancel !== "function") return;
+    const confirmAction = () => {
+        if (confirm(message)) onConfirm();
+        else onCancel();
+    };
+    return confirmAction;
+};
+
+const App = () => {
+    const deleteWorld = () => console.log("deleting world");
+    const confirmDelete = useConfirm("Are you sure?", deleteWorld);
+    return (
+        <div className="App">
+            <button onClick={confirmDelete}>Delete the World</button>
+        </div>
+    );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
