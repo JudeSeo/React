32a8102e21c76bae10eb2737bdf7fb91c3f4b013
JudeSeo <gn0408@gmail.com>
2022-05-16 15:34:00 +0900

    트위터 클론코딩 로그아웃 구현

--- src/twitter/components/Router.js
+++ src/twitter/components/Router.js
@@ -1,22 +1,31 @@
 import React, {useState} from "react";
-import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import {HashRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
+import Profile from "../routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = ({isLoggedIn}) => {
-    return (
-        <Router>
-            <Switch>
-                {isLoggedIn ? <>
+    return (<Router>
+        {isLoggedIn && <Navigation/>}
+        <Switch>
+            {isLoggedIn ? (
+                <>
                     <Route exact path="/">
-                        <Home />
+                        <Home/>
                     </Route>
-                </> : <Route exact path="/">
-                    <Auth/>
-                </Route>}
-            </Switch>
-        </Router>
-    )
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
+                </>) : (
+                <>
+                    <Route exact path="/">
+                        <Auth/>
+                    </Route>
+                </>
+            )}
+        </Switch>
+    </Router>)
 }
 
 export default AppRouter;
--- src/twitter/routes/Auth.js
+++ src/twitter/routes/Auth.js
@@ -30,9 +30,9 @@ const Auth = () => {
     const onSocialClick = async(event) => {
         const {target: {name}} = event;
         let provider;
-        if (name == "google")
+        if (name === "google")
             provider = new firebaseInstance.auth.GoogleAuthProvider();
-        else if (name == "github")
+        else if (name === "github")
             provider = new firebaseInstance.auth.GithubAuthProvider();
         const data = await authService.signInWithPopup(provider)
         console.log(data);
