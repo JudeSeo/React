bf120db1351abf4627ce72ca02f9ad0c93f9de08
JudeSeo <gn0408@gmail.com>
2022-05-18 15:34:29 +0900

    트위터 클론코딩 트윗출력 구현

--- src/twitter/routes/Home.js
+++ src/twitter/routes/Home.js
@@ -1,26 +1,47 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {dbService} from "../fBase";
 
 const Home = () => {
-    const [tweet, setTweet] = useState("");
+    const [twit, setTwit] = useState("");
+    const [twits, setTwits] = useState([]);
+    const getTwits = async () => {
+        const dbTwits = await dbService.collection("twit").get();
+        dbTwits.forEach(document => {
+            const twitObject = {
+                ...document.data(),
+                id: document.id,
+            }
+            setTwits(prev => [twitObject, ...prev])
+        });
+    }
+    useEffect(() => {
+        getTwits();
+    }, [])
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.collection("twit").add({
-            tweet: tweet,
+            twit,
             createdAt: Date.now()
         });
-        setTweet("");
+        setTwit("");
     }
     const onChange = event => {
         const {target: {value}} = event;
-        setTweet(value);
+        setTwit(value);
     };
     return (
         <div>
             <form onSubmit={onSubmit}>
-                <input type="text" value={tweet} onChange={onChange} placeholder="What's on your mind" maxLength={120}/>
+                <input type="text" value={twit} onChange={onChange} placeholder="What's on your mind" maxLength={120}/>
                 <input type="submit" value="Twit"/>
             </form>
+            <div>
+                {twits.map(twit =>
+                    <div key={twit.id}>
+                        <h4>{twit.twit}</h4>
+                    {/*    왜 두개씩 출력되는지 모르겠다... */}
+                    </div>)}
+            </div>
         </div>
     )
 }
