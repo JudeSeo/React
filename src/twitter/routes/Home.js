f8f7397a5b872a3c100f26d2b43b5e0d74fdb418
JudeSeo <gn0408@gmail.com>
2022-05-19 16:46:24 +0900

    트위터 클론코딩 사진업로드 로직 추가

--- src/twitter/routes/Home.js
+++ src/twitter/routes/Home.js
@@ -5,6 +5,7 @@ import Twit from "../components/Twit";
 const Home = ({userObj}) => {
     const [twit, setTwit] = useState("");
     const [twits, setTwits] = useState([]);
+    const [attachment, setAttachment] = useState()
     useEffect(() => {
         dbService.collection("twits").onSnapshot((snapshot) => {
             const twitArray = snapshot.docs.map((doc) => ({
@@ -24,10 +25,28 @@ const Home = ({userObj}) => {
         const {target: {value}} = event;
         setTwit(value);
     };
+    const onFileChange = (event) => {
+        const{target:{files}} = event;
+        const theFile = files[0];
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget:{result}} = finishedEvent
+            setAttachment(result);
+        }
+        reader.readAsDataURL(theFile);
+    }
+    const onClearAttachment = () => setAttachment(null);
     return (<div>
             <form onSubmit={onSubmit}>
                 <input type="text" value={twit} onChange={onChange} placeholder="What's on your mind" maxLength={120}/>
+                <input type="file" accept="image/*" onChange={onFileChange}/>
                 <input type="submit" value="Twit"/>
+                {attachment && (
+                    <div>
+                        <img src={attachment} width="50px" height="50px"/>
+                        <button onClick={onClearAttachment}>Clear</button>
+                    </div>
+                )}
             </form>
             <div>
                 {twits.map(twit => (<Twit key={twit.id} twitObj={twit} isOwner={twit.creatorId === userObj.uid}/>))}
