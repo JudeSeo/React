f0f7e05bafd57a3fd57312a797ae05751613d94e
JudeSeo <gn0408@gmail.com>
2022-05-19 13:22:42 +0900

    트위터 클론코딩 2번씩 렌더링 되는 문제 수정 반영

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
+    // <React.StrictMode>
+    <App/>
+    // </React.StrictMode>
 );
 
+// StrictMode로 App을 감싸고 있으면 개발모드에서 (개발 단계시 오류를 잘 잡기위해) 두 번씩 렌더링한다.
