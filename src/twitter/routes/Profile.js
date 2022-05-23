330fd6500e4f065361970464bf9db95cc6749573
JudeSeo <gn0408@gmail.com>
2022-05-23 14:32:55 +0900

    트위터 클론코딩 프로필 수정 로직 추가

--- src/twitter/routes/Profile.js
+++ src/twitter/routes/Profile.js
@@ -1,14 +1,38 @@
-import React from "react";
-import {authService} from "../fBase";
+import React, {useState, useEffect} from "react";
+import {authService, dbService} from "../fBase";
 import {useHistory} from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({userObj}) => {
     const history = useHistory();
+    const [newDisPalyName, setNewDisplayName] = useState(userObj.displayName)
     const onLogOutClick = () => {
         authService.signOut();
         history.push("/")
     }
+    const onChange = (event) => {
+        const {
+            target: {value}
+        } = event;
+        setNewDisplayName(value)
+    }
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        if (userObj.displayName !== newDisPalyName) {
+            const response = await userObj.updateProfile({displayName: newDisPalyName})
+        }
+    }
+    const getMyTwit = async () => {
+        const twits = await dbService.collection("twits").where("creatorId", "==", userObj.uid).orderBy("createdAt", "desc").get();
+        // console.log(twits.docs.map((doc)=>doc.data()))
+    }
+    useEffect(() => {
+        getMyTwit();
+    }, [])
     return (<>
+        <form onSubmit={onSubmit}>
+            <input type="text" placeholder="Display name" onChange={onChange}/>
+            <input type="submit" value="Update profile"/>
+        </form>
         <button onClick={onLogOutClick}>Log Out</button>
     </>)
 }
