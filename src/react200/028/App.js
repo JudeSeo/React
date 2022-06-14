b38cf4accb1462b0fe8611862d49156a3748896b
JudeSeo <gn0408@gmail.com>
2022-06-14 13:30:06 +0900

    리액트 200제 028

---
+++ src/react200/028/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import PureCmponentClass from "./Component";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>CSS 적용하기</p>
+            <PureCmponentClass />
+        </div>
+    );
+}
+
+export default App;
