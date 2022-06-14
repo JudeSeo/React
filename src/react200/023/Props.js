73173cab814e5f4a8b3caca49b26548a0faa539e
JudeSeo <gn0408@gmail.com>
2022-06-14 10:44:33 +0900

    리액트 200제 023

---
+++ src/react200/023/Props.js
@@ -0,0 +1,10 @@
+import React from "react";
+import datatype from "prop-types";
+
+const PropsNode = ({children}) => {
+    return (<div style={{padding: "0px"}}>
+        {children}
+    </div>)
+}
+
+export default PropsNode;
