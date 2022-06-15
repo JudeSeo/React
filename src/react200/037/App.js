76e213968e2fcd4eb32efeed5fb39be57b5e0bbc
JudeSeo <gn0408@gmail.com>
2022-06-16 08:44:20 +0900

    리액트 200제 037

---
+++ src/react200/037/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactstrapDropdown from "./Component";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>CSS 적용하기</p>
+            <ReactstrapDropdown />
+        </div>
+    );
+}
+
+export default App;
