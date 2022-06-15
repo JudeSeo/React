b80616a099f0d38fd8b4580108b767fc18169a3f
JudeSeo <gn0408@gmail.com>
2022-06-15 09:04:37 +0900

    리액트 200제 035

---
+++ src/react200/035/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css"
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    // <React.StrictMode>
+    <App/>
+    // </React.StrictMode>
+);
