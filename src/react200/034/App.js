24abacebb02adf0535fdd39c89594d4baa0e4495
JudeSeo <gn0408@gmail.com>
2022-06-15 09:04:25 +0900

    리액트 200제 034

---
+++ src/react200/034/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactstrapAlerts from "./Component";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>CSS 적용하기</p>
+            <ReactstrapAlerts />
+        </div>
+    );
+}
+
+export default App;
