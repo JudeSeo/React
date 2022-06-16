9a9a8dd2eacaebb0832e3e36d12d5a87d8881d02
JudeSeo <gn0408@gmail.com>
2022-06-16 09:27:34 +0900

    리액트 200제 040

---
+++ src/react200/040/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactstrapCard from "./Component";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>CSS 적용하기</p>
+            <ReactstrapCard />
+        </div>
+    );
+}
+
+export default App;
