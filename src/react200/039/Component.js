5907493a7c09abf914871ce700c3dfdd55b81eb0
JudeSeo <gn0408@gmail.com>
2022-06-16 09:27:22 +0900

    리액트 200제 039

---
+++ src/react200/039/Component.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+import {Button, ButtonGroup} from "reactstrap";
+
+const ReactstrapButtons = () => {
+    return (<div style={{padding: "0px"}}>
+        <Button color="primary">blue</Button>
+        <Button color="info">teal</Button>
+        <Button color="success">green</Button>
+        <Button color="warning">yellow</Button>
+        <Button color="danger">red</Button>
+        <Button color="dark">dark grey</Button>
+        <Button color="secondary">grey</Button>
+        <Button color="light">white</Button>
+    </div>)
+}
+export default ReactstrapButtons;
