c85b269351294e555e60ac7e7e809f4870cdd2da
JudeSeo <gn0408@gmail.com>
2022-06-16 10:04:29 +0900

    리액트 200제 043

---
+++ src/react200/043/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactstrapFade from "./Component";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>CSS 적용하기</p>
+            <ReactstrapFade />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/react200/043/Component.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import {Button, Fade} from "reactstrap";
+
+const ReactstrapCollapse = () => {
+    const [fadeInOut, setFadeInOut] = useState(true);
+    const toggle = () => {
+        setFadeInOut(!fadeInOut);
+    }
+    return (<div>
+        <Button color="success" onClick={toggle}>Fade In/Out</Button>
+        <Fade in={fadeInOut} tag="h1">Lorem ipsum sit amet, consectetur adipiscing elit</Fade>
+    </div>)
+}
+export default ReactstrapCollapse;
