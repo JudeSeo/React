df194e215b263cee979f83a588db1f534f4ecff8
JudeSeo <gn0408@gmail.com>
2022-06-17 11:22:45 +0900

    리액트 200제 054

---
+++ src/react200/054/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactTable from "./Component";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Start React 200!</h1>
+            <p>CSS 적용하기</p>
+            <ReactTable />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/react200/054/Component.js
@@ -0,0 +1,33 @@
+import React, {Component, useState, useEffect} from "react";
+import {Table} from "reactstrap";
+
+const ReactTable = () => {
+    return (<>
+        <Table>
+            {/* <Table dark bordered>
+            <Table striped hover>
+            <Table borderless size="sm"> */}
+            <thead>
+            <tr>
+                <th>number</th>
+                <th>Book name</th>
+                <th>Price</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>React 100</td>
+                <td>₩10000</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>React 200</td>
+                <td>₩20000</td>
+            </tr>
+            </tbody>
+        </Table>
+    </>)
+}
+
+export default ReactTable;
