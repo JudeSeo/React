2820bc48c012b1dc6a5fc78e51a58e6fe49b4161
JudeSeo <gn0408@gmail.com>
2022-06-23 09:12:31 +0900

    리액트 200제 081-083

---
+++ src/react200/081-083/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import "./App.css";
+import App from "./App";
+import { createStore, applyMiddleware } from "redux";
+import {Provider} from "react-redux";
+import reducers from "./reducers";
+import "bootstrap/dist/css/bootstrap.css";
+
+// const CallMiddleware = store => nextMiddle=>action=>{
+//   console.log("1. reducer 실행 전")
+//   console.log(`2. action.type: ${action.type}, store str: ${store.getState().data.str}`);
+//   let result = nextMiddle(action);
+//   console.log("3. reducer 실행 후")
+//   console.log(`4. action.type: ${action.type}, store str: ${store.getState().data.str}`);
+//   return result;
+// }
+//
+// const store = createStore(reducers, applyMiddleware(CallMiddleware));
+const store = createStore(reducers)
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+
+const listener = () => {
+  root.render(
+      <Provider store={store}>
+        <App indexProp="react" />
+      </Provider>
+  );
+};
+store.subscribe(listener);
+listener();
+
+// const rootElement = document.getElementById('root');
+// const root = ReactDOM.createRoot(rootElement);
+
+// root.render(
+// <React.StrictMode>
+// <App/>
+// </React.StrictMode>
+// );
