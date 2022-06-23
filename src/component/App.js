5894664347acf98b4db880ae2987f7b562ebd459
JudeSeo <gn0408@gmail.com>
2022-06-23 11:32:06 +0900

    리액트 200제 기본

---
+++ src/component/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+import "../css/new.css";
+import HeaderAdmin from "./Header/Header admin";
+import Footer from "./Footer/Footer";
+import LoginForm from "./LoginForm";
+
+const App = () => {
+  return (
+    <div className="App">
+      <HeaderAdmin />
+      <Route exact path="/" component={LoginForm} />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/twitter/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import "./styles.css";
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    // <React.StrictMode>
+    <App/>
+    // </React.StrictMode>
+);
+
+// StrictMode로 App을 감싸고 있으면 개발모드에서 (개발 단계시 오류를 잘 잡기위해) 두 번씩 렌더링한다.
